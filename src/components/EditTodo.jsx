363f0afd4f6725a874ec594737de99ae444eb69a
AnasBouarramou <anas.bouarramou@gmail.com>
2025-09-19 17:52:12 +0200

    2e commit

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -1,5 +1,45 @@
-function EditTodo() {
-  return <h2>Edit Todo</h2>;
-}
+import { useState } from "react";
 
+function EditTodo({ todo, editTodo, cancelEditTodo }) {
+  const [value, setValue] = useState(todo.content);
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  }
+
+  function handleClick() {
+    if (value.length) {
+      editTodo(value);
+      setValue("");
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.code === "Enter" && value.length) {
+      handleClick();
+    }
+  }
+
+  return (
+    <div className="d-flex flex-row justify-content-center align-items-center mb-10">
+      <input
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        type="text"
+        placeholder="Ajouter une todo"
+        className="mr-15 flex-fill"
+      />
+      <button
+        onClick={cancelEditTodo}
+        className="btn btn-reverse-primary mr-15"
+      >
+        Annuler
+      </button>
+      <button onClick={handleClick} className="btn btn-primary">
+        Sauvegarder
+      </button>
+    </div>
+  );
+}
 export default EditTodo;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,15 @@
-function TodoItem({ todo, deleteTodo }) {
+function TodoItem({ todo, deleteTodo, toggleTodo, editTodo }) {
   return (
     <li className="mb-10 d-flex flex-row justify-content-center align-items-center p-10">
-      <span className="flex-fill mr-15">{todo.content}</span>
-      <button className="btn btn-primary mr-15">Valider</button>
-      <button className="btn btn-primary mr-15">Modifier</button>
+      <span className="flex-fill mr-15">
+        {todo.content} {todo.done && "(✓)"}{" "}
+      </span>
+      <button className="btn btn-primary mr-15" onClick={toggleTodo}>
+        Valider
+      </button>
+      <button onClick={editTodo} className="btn btn-primary mr-15">
+        Modifier
+      </button>
       <button onClick={deleteTodo} className="btn btn-reverse-primary mr-15">
         Supprimer
       </button>
